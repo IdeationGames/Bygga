972a061407787f161e54cff101847f4f7bcf012b
TheCell <simon_hischier@hotmail.com>
2017-11-19 18:06:25 +0100

    added default scene with movable head

---
+++ Bygga/Assets/Scripts/headMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class headMove : MonoBehaviour
+{
+	private Rigidbody2D rigidBody;
+	private float horizontalMove = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigidBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		horizontalMove = Input.GetAxis("Horizontal") * 2;
+
+		Vector2 currentPosition = transform.position;
+		// linear functino: y = mx + b; 0.28f is a trial and error result
+		Vector2 moveDirection = new Vector2(horizontalMove * Time.deltaTime, -1 * 0.28f * horizontalMove * Time.deltaTime);
+		rigidBody.MovePosition(currentPosition + moveDirection);
+	}
+}
---
+++ Bygga/Assets/Scripts/spawnBlocks.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnBlocks : MonoBehaviour
+{
+	public List<GameObject> startElements = new List<GameObject>();
+	private List<GameObject> undroppedElements = new List<GameObject>();
+	private List<GameObject>.Enumerator pinselEnumerator;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
