82179da8b7745fe72f3cbc673a87249a19b72871
Simon <simon_hischier@hotmail.com>
2017-12-16 17:33:38 +0100

    added scorecomparison and Effects

---
+++ Bygga/Assets/Scripts/comparePlacement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class comparePlacement : MonoBehaviour
+{
+    public bool runComparison = true;
+
+    private GameObject currentFragment;
+    private bool isResting
+    {
+        get; set;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentFragment = this.gameObject;
+        isResting = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (runComparison && isResting)
+        {
+            SpriteRenderer[] allSpriteObjs = FindObjectsOfType<SpriteRenderer>();
+            SpriteRenderer currentSpriteRenderer = currentFragment.GetComponent<SpriteRenderer>();
+            for (int i = 0; i < allSpriteObjs.Length; i++)
+            {
+                if (currentSpriteRenderer.sprite == allSpriteObjs[i].sprite
+                    && !allSpriteObjs[i].Equals(currentSpriteRenderer))
+                {
+                    float distance = Vector2.Distance(allSpriteObjs[i].transform.position, currentSpriteRenderer.transform.position);
+                    globalInfo.gameScoreFromDistance(distance);
+                    runComparison = false;
+
+                    GameObject explosion1 = (GameObject)Instantiate(
+                        Resources.Load("Prefabs" + Path.DirectorySeparatorChar + "Effects" + Path.DirectorySeparatorChar + "explosionAnim")
+                    );
+                    GameObject explosion2 = (GameObject)Instantiate(
+                        Resources.Load("Prefabs" + Path.DirectorySeparatorChar + "Effects" + Path.DirectorySeparatorChar + "explosionAnim")
+                    );
+                    GameObject explosion3 = (GameObject)Instantiate(
+                        Resources.Load("Prefabs" + Path.DirectorySeparatorChar + "Effects" + Path.DirectorySeparatorChar + "explosionAnim")
+                    );
+
+                    Vector3 positionBottomLeft = currentSpriteRenderer.bounds.center - currentSpriteRenderer.bounds.extents;
+                    Vector3 positionBottomMiddle = currentSpriteRenderer.bounds.center - currentSpriteRenderer.bounds.extents;
+                    Vector3 positionBottomRight = currentSpriteRenderer.bounds.center - currentSpriteRenderer.bounds.extents;
+                    positionBottomMiddle.x += currentSpriteRenderer.bounds.extents.x;
+                    positionBottomRight.x += currentSpriteRenderer.bounds.extents.x * 2;
+
+                    explosion1.transform.position = positionBottomLeft;
+                    explosion2.transform.position = positionBottomMiddle;
+                    explosion3.transform.position = positionBottomRight;
+                }
+            }
+        }
+        
+        if (currentFragment.GetComponent<Rigidbody2D>().IsSleeping())
+        {
+            isResting = true;
+        }
+	}
+}
---
+++ Bygga/Assets/Scripts/globalInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalInfo : MonoBehaviour
+{
+    private static int gameScore = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    /*
+    void updateGameScore(int addAmount)
+    {
+        gameScore += addAmount;
+    }
+    */
+
+    public static void gameScoreFromDistance(float distance)
+    {
+        if (distance < 1.0f)
+        {
+            float reverse = 100 * (1.0f - distance);
+            gameScore += (int) reverse;
+        }
+
+        Debug.Log(gameScore);
+    }
+
+    public static int getGameScore()
+    {
+        return gameScore;
+    }
+}
