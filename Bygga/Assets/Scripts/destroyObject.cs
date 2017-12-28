5bd0471a7f93d9cfb617c1044989538923efd6dc
michael schürmann <michaelschuermann@hotmail.com>
2017-12-28 18:32:15 +0100

    some changes and testing

---
+++ Bygga/Assets/Scripts/destroyObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyObject : MonoBehaviour
+{
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		//Check collision name
+		Debug.Log ("collision name = " + col.gameObject.name);
+		if (col.gameObject.name == "fragment_01 (1)") {
+			Destroy (col.gameObject);
+		}
+	}
+}
